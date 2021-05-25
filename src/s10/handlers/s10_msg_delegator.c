135162f8a9de3d0f64180f7f9f967649a5e3bb46
NamrataG <namrata.ghodeswar@gmail.com>
2021-05-25 10:26:39 +0530

    Case added:s10_ID_request_handler

--- src/s10/handlers/s10_msg_delegator.c
+++ src/s10/handlers/s10_msg_delegator.c
@@ -182,6 +182,10 @@ handle_s10_message(void *message)
 	case GTP_IDENTIFICATION_RES:
 		s10_ID_resp_handler(msgBuf_p, &msgHeader, tmme_ip);
 		break;
+		
+	case GTP_IDENTIFICATION_REQ:
+		s10_ID_request_handler(msgBuf_p, &msgHeader, tmme_ip);
+		break;
 	case GTP_FORWARD_ACCESS_CONTEXT_NOTIFICATION:
 		S10_fwd_acc_ctxt_noti_handler(msgBuf_p,&msgHeader,tmme_ip);
 	case GTP_FORWARD_RELOCATION_CMP_ACK:
